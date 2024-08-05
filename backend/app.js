ecdaad0ce74a4cf72ffbd3c4f569e3ff4b5636aa
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-08-05 22:09:52 +0900

    feat : app.js

---
+++ backend/app.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const cors = require("cors");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const passport = require("passport");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const path = require("path");
+const hpp = require("hpp");
+const helmet = require("helmet");
+
+dotenv.config();
+const app = express();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(morgan("combined"));
+  app.use(hpp());
+  app.use(helmet({ contentSecurityPolicy: false }));
+  app.use(
+    cors({
+      origin: "http://nodebird.com",
+      credentials: true,
+    })
+  );
+} else {
+  app.use(morgan("dev"));
+  app.use(
+    cors({
+      origin: true,
+      credentials: true,
+    })
+  );
+}
+
+app.use("/", express.static(path.join(__dirname, "uploads")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    saveUninitialized: false,
+    resave: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+      domain: process.env.NODE_ENV === "production" && ".nodebird.com",
+    },
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get("/", (req, res) => {
+  res.send("hello express");
+});
+
+app.listen(4000, () => {
+  console.log("서버 실행 중!");
+});
