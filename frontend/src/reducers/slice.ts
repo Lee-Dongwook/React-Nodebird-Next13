8219c99b37e4f90e8c39a8b9b4beb0d96f6ad997
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-08-06 01:03:46 +0900

    feat : Redux 스토어 설정

---
+++ frontend/src/reducers/slice.ts
@@ -0,0 +1,38 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+import User from "@/typings/user";
+
+export interface GlobalState {
+  snackBar: {
+    message: string;
+    isOpen: boolean;
+  };
+}
+
+const initialState: GlobalState = {
+  snackBar: {
+    message: "",
+    isOpen: false,
+  },
+};
+
+export const globalSlice = createSlice({
+  name: "global",
+  initialState,
+  reducers: {
+    openSnackBar: (state) => {
+      state.snackBar.isOpen = true;
+    },
+    closeSnackBar: (state) => {
+      state.snackBar.isOpen = false;
+    },
+    setSnackBarMessage: (state, action: PayloadAction<string>) => {
+      state.snackBar.message = action.payload;
+    },
+  },
+});
+
+export const { openSnackBar, closeSnackBar, setSnackBarMessage } =
+  globalSlice.actions;
+
+export default globalSlice.reducer;
